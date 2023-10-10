9e1f645e8a0c6480cd6a0d6c6d9d291c935fa7f6
MorboVenchi <hiphop1884@gmail.com>
2023-10-10 23:39:59 +0300

    Notes RA
    Notes RA

---
+++ Notes/Models/About.cs
@@ -0,0 +1,9 @@
+﻿namespace Notes.Models;
+
+internal class About
+{
+    public string Title => AppInfo.Name;
+    public string Version => AppInfo.VersionString;
+    public string MoreInfoUrl => "https://career.severstal.com/directions/graduates/it_hub/?utm_source=online&utm_medium=internal&utm_campaign=references";
+    public string Message => "Сделано с использованием .NET MAUI.";
+}
